3480b834eefd1fc93716dcdafe6d02f7f0d2e443
braghunathan6 <braghunathan6@gatech.edu>
2025-02-13 20:35:27 -0500

    Fix requirements, update config to include Redis password

--- llm-engine/app/RedisManager.py
+++ llm-engine/app/RedisManager.py
@@ -2,17 +2,19 @@ import redis
 
 
 class RedisManager:
-    def __init__(self, host_, port_):
+    def __init__(self, host_, port_, password_ = None):
         """
         Initialize the Redis connection.
 
         Args:
             host (str): The host url for redis.
             port (int): The port for the redis connection
+            password (str, Optional): The password for the redis connection
         """
         redis_client = redis.Redis(
             host=host_,
             port=port_,
+            password=password_,
             db=0,
             decode_responses=True
         )
